f806b9325c9c6c69c7870f240862d54a8d870dac
Mahruf Hossain <88084890+MahrufHossain@users.noreply.github.com>
2025-07-01 23:44:08 +0600

    Updated the code to introduce a flag system

--- arduino_programming/codes/simple_traffic_system/main.c
+++ arduino_programming/codes/simple_traffic_system/main.c
@@ -18,30 +18,30 @@ int main(int argc, char *argv[])
   // address the floating pin issue by pulling up the push switch
   PORTB |= (1 << PUSH_SWITCH);
  
-  while (1) {//the first loop to involve road-crossing
- 
-    while (1) {  //second loop for normal traffic mechanism
-      if (!(PINB & (1 << PUSH_SWITCH))) {
-        break; //if push switch is pressed break the loop after finishing the current time frame of the lights
-      }
+  while (1) {//the first loop to involve road-crossingchar
+    char flag = 0;
+      while (1) {  //second loop for normal traffic mechanism
+        if (!(PINB & (1 << PUSH_SWITCH))) {
+          flag = 1;
+          break;//if push switch is pressed break the loop after finishing the current time frame of the lights
+        }
   
-      else {
-        PORTD |= (1 << RED_LED); //set high 
-        _delay_ms(2000);
-        PORTD &= ~(1 << RED_LED); //set low
-        PORTD |= (1 << GREEN_LED);
-        _delay_ms(2000);
-        PORTD &= ~(1 << GREEN_LED);
-        PORTD |= (1 << YELLOW_LED);
-        _delay_ms(1000);
-        PORTD &= ~(1 << YELLOW_LED);
+        else {
+          PORTD |= (1 << RED_LED); //set high 
+          _delay_ms(2000);
+          PORTD &= ~(1 << RED_LED); //set low
+          PORTD |= (1 << GREEN_LED);
+          _delay_ms(2000);
+          PORTD &= ~(1 << GREEN_LED);
+          PORTD |= (1 << YELLOW_LED);
+          _delay_ms(1000);
+          PORTD &= ~(1 << YELLOW_LED);
+        }
       }
-    
-    PORTD |= (1 << RED_LED); // break after the full cycle
-
-    }
-
-    _delay_ms(5000); //give enough time to cross the road
+    if (flag == 1) {
+      PORTD |= (1 << RED_LED); // break after the full cycle
+      _delay_ms(5000); //give enough time to cross the road
+      }
   }
   
   return 0;
