0fcfa7d5b7b4ee0c29c29a462a31fbd2f7843f19
Mahruf Hossain <88084890+MahrufHossain@users.noreply.github.com>
2025-07-02 18:41:34 +0600

    Modified the main.c file to introduce state machine logic for better code

--- arduino_programming/codes/simple_traffic_system/main.c
+++ arduino_programming/codes/simple_traffic_system/main.c
@@ -7,41 +7,80 @@
 #define GREEN_LED PD5
 #define YELLOW_LED PD6
 
-int main(int argc, char *argv[])
-{ 
-  //set data directional register for output pins
-  DDRD |= (1 << RED_LED);
-  DDRD |= (1 << GREEN_LED);
-  DDRD |= (1 << YELLOW_LED);
+typedef enum {
+  STATE_GREEN_TRAFFIC,
+  STATE_RED_TRAFFIC,
+  STATE_YELLOW_TRAFFIC,
+  STATE_PEDESTRIAN_CROSS
+} TrafficLightState;
+
+TrafficLightState current_state = STATE_RED_TRAFFIC;
+
+volatile uint8_t pedestrian_requested = 0;
+
+void setup() {
+  DDRD |= ((1 << RED_LED) | (1 << GREEN_LED) | (1 << YELLOW_LED));
   //set data directional register for input pins
   DDRB &= ~(1 << PUSH_SWITCH);
   // address the floating pin issue by pulling up the push switch
   PORTB |= (1 << PUSH_SWITCH);
- 
-  while (1) {//the first loop to involve road-crossingchar
-    char flag = 0;
-      while (1) {  //second loop for normal traffic mechanism
-        if (!(PINB & (1 << PUSH_SWITCH))) {
-          flag = 1;
-          break;//if push switch is pressed break the loop after finishing the current time frame of the lights
+
+}
+int main(int argc, char *argv[])
+{ 
+  //set data directional register for output pins
+  setup();
+
+  while (1) {         
+    if (!(PINB & (1 << PUSH_SWITCH))) {
+      _delay_ms(50);
+      if (!(PINB & (1 << PUSH_SWITCH))) {
+        pedestrian_requested = 1;
+      }
+    }
+
+    switch (current_state) {
+      case STATE_GREEN_TRAFFIC:
+        PORTD |= (1 << GREEN_LED);
+        PORTD &= ~((1 << RED_LED) | (1 << YELLOW_LED));
+        _delay_ms(2000);
+
+
+        current_state = STATE_YELLOW_TRAFFIC;
+        break;
+      
+      case STATE_YELLOW_TRAFFIC:
+        PORTD |= (1 << YELLOW_LED);
+        PORTD &= ~((1 << RED_LED) | (1 << GREEN_LED));
+        _delay_ms(1000);
+
+        current_state = STATE_RED_TRAFFIC;
+        break;
+      
+      case STATE_RED_TRAFFIC:
+        PORTD |= (1 << RED_LED);
+        PORTD &= ~((1 << GREEN_LED) | (1 << YELLOW_LED));
+        _delay_ms(2000);
+
+        if (pedestrian_requested == 1) {
+          current_state = STATE_PEDESTRIAN_CROSS;
+          pedestrian_requested = 0;
         }
-  
         else {
-          PORTD |= (1 << RED_LED); //set high 
-          _delay_ms(2000);
-          PORTD &= ~(1 << RED_LED); //set low
-          PORTD |= (1 << GREEN_LED);
-          _delay_ms(2000);
-          PORTD &= ~(1 << GREEN_LED);
-          PORTD |= (1 << YELLOW_LED);
-          _delay_ms(1000);
-          PORTD &= ~(1 << YELLOW_LED);
+          current_state = STATE_GREEN_TRAFFIC;
+
         }
-      }
-    if (flag == 1) {
-      PORTD |= (1 << RED_LED); // break after the full cycle
-      _delay_ms(5000); //give enough time to cross the road
-      }
+        break;
+      
+
+      case STATE_PEDESTRIAN_CROSS:
+        PORTD |= (1 << RED_LED);
+        PORTD &= ~((1 << GREEN_LED) | (1 << YELLOW_LED));
+
+        _delay_ms(5000);
+        current_state = STATE_GREEN_TRAFFIC;
+        break;    
+    }
   }
   
   return 0;
